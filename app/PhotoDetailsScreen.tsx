230d870ddd9c30e00dccc026977205f9513a72f5
Seth Akuoko <sethakuoko123@gmail.com>
2025-06-05 20:44:06 +0000

    Implemented the design for the home screen

---
+++ app/PhotoDetailsScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const PhotoDetailsScreen = () => {
+  return (
+    <View>
+      <Text>PhotoDetailsScreen</Text>
+    </View>
+  );
+};
+
+export default PhotoDetailsScreen;
---
+++ app/ScanScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function ScanScreen() {
+  return (
+    <View>
+      <Text> This is the Scan Screen</Text>
+    </View>
+  );
+}
